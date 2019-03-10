876ceae30446b58456781a638275f7645508be31
Pip <Thetardis101@hotmail.com>
2019-03-10 16:45:23 +0000

    Made Script to randomize starting player skin.

---
+++ Assets/playerSkinRandomizerScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerSkinRandomizerScript : MonoBehaviour
+{
+    public static int playerone;
+    public static int playertwo;
+    
+    void Start()
+    {
+        playerone = Random.Range(1, 6);
+        Debug.Log(playerone);
+        playertwo = Random.Range(1, 6);
+        Debug.Log(playertwo);
+        //I Know how messy this is, but it's just a really rough draft for it.
+        if(playertwo == playerone)
+        {
+            playertwo = Random.Range(1, 6);
+            Debug.Log(playertwo);
+        }
+        if (playertwo == playerone)
+        {
+            playertwo = Random.Range(1, 6);
+            Debug.Log(playertwo);
+        }
+        if (playertwo == playerone)
+        {
+            playertwo = Random.Range(1, 6);
+            Debug.Log(playertwo);
+        }
+
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/playerSpriteManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerSpriteManager : MonoBehaviour
+{
+    public Sprite[] spriteList;
+    public int playernumber;
+    bool setup = false;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (!setup)
+        {
+            if (playernumber == 1)
+            {
+                this.GetComponent<SpriteRenderer>().sprite = spriteList[playerSkinRandomizerScript.playerone];
+            }
+            if (playernumber == 2)
+            {
+                this.GetComponent<SpriteRenderer>().sprite = spriteList[playerSkinRandomizerScript.playertwo];
+            }
+            setup = true;
+        }
+    }
+}
