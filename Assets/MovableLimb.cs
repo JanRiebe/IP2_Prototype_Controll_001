f0fdda6e049ffc179248ab73a1cd09e66f29044e
Jan Riebe <corone.et.monedula@gmail.com>
2019-03-11 13:39:49 +0000

    Revert "Merge branch 'Development' into Development-Copy"
    This reverts commit 2aab1a52fc439aeb6bd863251feec02ffdb89bd0, reversing
    changes made to c8cd3a74ff04aa5345545bb786ed9b5341c76d94.

--- Assets/MovableLimb.cs
+++ Assets/MovableLimb.cs
@@ -11,7 +11,18 @@ public abstract class MovableLimb: MonoBehaviour
 		Initialise();
 	}
 
-
+    [System.Serializable]
+    public class ActivationAxis
+    {
+        [Tooltip("The name of the axis that switches this limb on.")]
+        public string name;
+        [Tooltip("How much this limb is afected by direction input after it has been activated by this axis.")]
+        [Range(0, 1)]
+        public float influence = 1.0f;
+    }
+    
+    public ActivationAxis[] activationInputs;
+	
 	[SerializeField]
     float strength;
 
@@ -19,12 +30,30 @@ public abstract class MovableLimb: MonoBehaviour
 
 
 
-    [SerializeField]
-    Color controlledCollor = Color.red;
-    Color basicColor;
+	
+    [System.Serializable]
+    public class Attack
+    {
+        [Tooltip("The time in seconds it takes the limb to reach its final force strength.")]
+        public float attackTime = 1;
+
+        [Tooltip("The amount of force that will be applied over the duration of attackTime.")]
+        public AnimationCurve attackOverTime;
+    }
+
+    [Tooltip("How forces act on the limb over time when the player makes an input.")]
+    public Attack attack;
+	
+
+	float timeOfTakingControl;	// The time at which the limb last started to be controlled by the player.
+
+
+    float forceFactor = 1;	// Determins if and how much a limb is currently controlled by directional input.
 
     protected Rigidbody2D rb;
 	
+	bool wasControlledInPreviousUpdate = false;		//TODO find more elegant solution
+
 
 	GruntOnDemand gruntOnDemand;
 
@@ -34,27 +63,72 @@ public abstract class MovableLimb: MonoBehaviour
 
     virtual protected void Initialise()
     {
-
-        // Saving the world space position and rotation at level start, to use them later in ResetToStartPosition.
-        startPosition = transform.position;
-        startRotation = transform.rotation;
-
-        basicColor = GetComponent<SpriteRenderer>().color;
-
         rb = GetComponent<Rigidbody2D>();
 		gruntOnDemand = GetComponent<GruntOnDemand>();
     }
+
+      // Update is called once per frame
+    void Update()
+    {
+        // Calculating the force factor.
+        forceFactor = 0.0f;
+        bool isControlledByPlayer = false;
+        foreach(ActivationAxis axis in activationInputs)
+        {
+            if (Input.GetAxis(axis.name) > 0.1f)
+            {
+                forceFactor += axis.influence;
+                isControlledByPlayer = true;
+            }
+        }
+        forceFactor = Mathf.Min(1.0f, forceFactor);
+
+        // Setting this to forward or backward kinematic, depending on whether any of the controlling axes are active.
+		if(wasControlledInPreviousUpdate != isControlledByPlayer)
+		{
+			SetControlled(isControlledByPlayer);
+			wasControlledInPreviousUpdate = isControlledByPlayer;
+		}
+		
+		if(isControlledByPlayer)
+		{
+			GetComponent<SpriteRenderer>().color = Color.red;
+		}
+		else
+			GetComponent<SpriteRenderer>().color = Color.white;
+
+        //TODO move these in input control script, that calls force functions
+		if(isControlledByPlayer)
+		{
+        if (Input.GetKey(KeyCode.UpArrow))
+            ForceDirection(Vector2.up);
+        if (Input.GetKey(KeyCode.DownArrow))
+            ForceDirection(Vector2.down);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            ForceDirection(Vector2.left);
+        if (Input.GetKey(KeyCode.RightArrow))
+            ForceDirection(Vector2.right);
+			}
+			/*
+        if (Input.GetKey(KeyCode.Q))
+            ForceRotation(true);
+        if (Input.GetKey(KeyCode.E))
+            ForceRotation(false);
+			*/
+    }
+
 
 
 	
 
     public virtual void SetControlled(bool controlled)
 	{
-        if (controlled)
-            GetComponent<SpriteRenderer>().color = controlledCollor;
-        else
-            GetComponent<SpriteRenderer>().color = basicColor;
-    }
+		if(controlled)
+		{
+			timeOfTakingControl = Time.time;
+			//gruntOnDemand.PleaseGruntNow();
+		}
+	}
 
     /// <summary>
     /// Call this to apply a force in a global direction, independently of the limbs own orientation.
@@ -64,14 +138,20 @@ public abstract class MovableLimb: MonoBehaviour
     /// Should not be of magnitude 0.</param>
     public void ForceDirection(Vector2 direction)
     {
-        rb.AddForce(direction.normalized * strength);
-
+        rb.AddForce(direction.normalized * strength * forceFactor/* * AttackRightNow()*/);
 		if(gruntOnDemand)
 			gruntOnDemand.PleaseGruntNow();
     }
 
     
-    
+
+	float AttackRightNow()
+	{
+		float timePassed = Time.time - timeOfTakingControl;
+		float progress = Mathf.Min(1.0f, timePassed/attack.attackTime);
+		return attack.attackOverTime.Evaluate(progress);
+	}
+
 
 	virtual protected void SwitchToIK(MovableLimb sender)
 	{
