893045977252595fdb23c421b94292484a75a992
Pip <Thetardis101@hotmail.com>
2019-03-11 12:37:58 +0000

    Re-Organization

---
+++ Assets/grabableSkinRadomizer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grabableSkinRadomizer : MonoBehaviour
+{
+    public Sprite[] skins;
+    void Start()
+    {
+        this.GetComponent<SpriteRenderer>().sprite = skins[Random.Range(0, skins.Length)]; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
