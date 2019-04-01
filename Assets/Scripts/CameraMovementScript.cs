e20baba8120b158c01d315f85198f33bec035e32
Goodmanc1998 <goodmanc1998@gmail.com>
2019-04-01 20:33:31 +0100

    Updating camera script
    Camera script now tracks the highest player, when the camera is resetting the death zone is desabled and then made active once its done.

--- Assets/Scripts/CameraMovementScript.cs
+++ Assets/Scripts/CameraMovementScript.cs
@@ -2,68 +2,92 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMovementScript : MonoBehaviour {
+public class CameraMovementScript : MonoBehaviour
+{
 
     //We essentially want to set the size to the amount of players that are playing. 
     //You can modify this part of the script to change size and input the appropriate Gameobjects a the start of the level. 
     //It's probably a good idea track camera using the head.
     public List<GameObject> playerlist = new List<GameObject>();
-    List<float> positions = new List<float>();
-    
-    //Float number used to store central position of players
-    float playerCentre;
-    //the adjusted amount the camera moves up by
-    public float heightAdjustment;
+
+    //Floats used for storing the highest players height and the Cameras movement speed
+    float highestPlayer;
+    public float speed;
+
 
     //Vectors used for cameras position
     Vector3 cameraStartPosition;
-    Vector3 currentCameraPosition;
-    
+
+    bool isResettingToStart = false;
+    public GameObject DeathZone;
+
     private void Awake()
     {
         //Storing the starting camera position
         cameraStartPosition = transform.position;
     }
-	
-	void Update ()
-    {
-        //Taking in the current camera position
-         currentCameraPosition = transform.position;
 
+    void OnEnable()
+    {
+        // Subscribing to round over events.
+        PlayerInGame.OnRoundOver += CameraReset;
+    }
+
+    void OnDisable()
+    {
+        PlayerInGame.OnRoundOver -= CameraReset;
+    }
+
+    void Update()
+    {
+        //Storing the the central position for the players
+        float playerCentre = 0;
         for (int i = 0; i < playerlist.Count; i++)
         {
-            //Storing the the central position for the players
-            playerCentre = playerCentre + playerlist[i].transform.position.y;
-            positions.Add(playerlist[i].transform.position.y);
-        }
-        
-        playerCentre = (playerCentre / playerlist.Count) + heightAdjustment;
-
-        //Storing the central position of players
-        Vector3 playerCentreV = new Vector3(transform.position.x, playerCentre, transform.position.z);
-
-        //Moving the cameras position to the central 
-        float t = 0;
-        while (t < 1)
-        {
-            t += Time.deltaTime;
-            if(playerCentreV.y > currentCameraPosition.y)
+            if (playerlist[i].transform.position.y > transform.position.y)
             {
-                transform.position = currentCameraPosition + (playerCentreV - currentCameraPosition) * t;
+                highestPlayer = playerlist[i].transform.position.y;
             }
         }
+        playerCentre = highestPlayer;
+
+        Vector3 target = new Vector3(transform.position.x, playerCentre, transform.position.z);
+
+        //Moving the cameras position to the central, if not currently resetting.   
+        if (playerCentre > transform.position.y && !isResettingToStart)
+        {
+            Debug.Log((target - transform.position) * Time.deltaTime * speed);
+            transform.position += (target - transform.position) * Time.deltaTime * speed;
+        }
     }
-    
+
+    void DeathZoneReset()
+    {
+        DeathZone.SetActive(false);
+    }
+
     //called to reset the cameras position at the bottom
     void CameraReset()
     {
-        float t = 0;
-        while (t < 1)
-        {
-            t += Time.deltaTime;
-            transform.position = currentCameraPosition - (currentCameraPosition - cameraStartPosition) * t;
-        }
-
+        StartCoroutine(MoveToStart());
     }
 
+    IEnumerator MoveToStart()
+    {
+        isResettingToStart = true;
+        DeathZone.SetActive(false);
+        float t = 0;
+        Vector3 lerpBegin = transform.position;
+        while (t < 1)
+        {
+            t += Time.deltaTime * speed;
+
+            transform.position = Vector3.Lerp(lerpBegin, cameraStartPosition, t);
+
+            yield return null;
+        }
+        transform.position = cameraStartPosition;
+        DeathZone.SetActive(true);
+        isResettingToStart = false;
+    }
 }
