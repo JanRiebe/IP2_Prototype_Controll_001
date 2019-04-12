9374fba63a3df1303b814ce2837c74b69ee735fe
Goodmanc1998 <goodmanc1998@gmail.com>
2019-04-12 11:43:15 +0100

    Updating art assets
    Background, mountain and grabables, player hands updated

--- Assets/grabableSkinRadomizer.cs
+++ Assets/grabableSkinRadomizer.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class grabableSkinRadomizer : MonoBehaviour
 {
+    static float rotationMax = 45;
+
     public Sprite[] skins;
     void Start()
     {
-        this.GetComponent<SpriteRenderer>().sprite = skins[Random.Range(0, skins.Length)]; 
+        this.GetComponent<SpriteRenderer>().sprite = skins[Random.Range(0, skins.Length)];
+        transform.rotation = Quaternion.Euler(0,0,Random.value*rotationMax-rotationMax/2);
     }
 
     // Update is called once per frame
