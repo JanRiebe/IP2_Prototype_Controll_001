f6b27263859da7ad8a8ce4f89714f9aff8b541c7
Jan Riebe <corone.et.monedula@gmail.com>
2019-04-13 16:16:29 +0100

    Bug fix: Hands holding on to air after resetting.

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -114,5 +114,14 @@ public class Hand : MovableLimb
 	}
 
 
+    
+    public override void ResetToStartPosition()
+    {
+        base.ResetToStartPosition();
+        _currentHandle = null;
+        SetControlled(false);
+        SwitchToFK(this);
+    }
+    
 
 }
