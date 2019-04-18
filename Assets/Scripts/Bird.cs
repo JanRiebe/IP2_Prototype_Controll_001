894ba5cff4f90c4fcde48f35816f7ee07dc9e0dd
Jan Riebe <corone.et.monedula@gmail.com>
2019-04-18 10:58:06 +0100

    Adding animated bird.

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public bool flying;
+    public float speed;
+
+
+    private void Start()
+    {
+        if(flying)
+            GetComponent<Animator>().SetTrigger("fly");
+    }
+
+    private void Update()
+    {
+        if (flying)
+            transform.Translate(Vector3.right * speed * Time.deltaTime, Space.World);
+
+        if (transform.position.x > 15 || transform.position.x < -15)
+            Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            GetComponent<Animator>().SetTrigger("fly");
+            flying = true;
+        }
+    }
+}
---
+++ Assets/Scripts/BirdSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdSpawner : MonoBehaviour
+{
+    public GameObject BirdPrefab;
+    public Vector2 spawnRandomBetween;
+    public float nextSpawnIn;
+
+    private void Start()
+    {
+        nextSpawnIn = Random.Range(spawnRandomBetween.x, spawnRandomBetween.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nextSpawnIn -= Time.deltaTime;
+
+        if(nextSpawnIn <= 0)
+        {
+            nextSpawnIn = Random.Range(spawnRandomBetween.x, spawnRandomBetween.y);
+            Instantiate(BirdPrefab, new Vector3(-13, Camera.main.transform.position.y + Random.Range(-5,5),-0.5f), Quaternion.identity);
+        }
+    }
+}
